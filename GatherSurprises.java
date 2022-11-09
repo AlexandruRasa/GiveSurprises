a6bd31625058540ffd22ee8f43dc047ef9c534be
AlexandruRașa <114824311+AlexandruRasa@users.noreply.github.com>
2022-11-09 04:31:09 +0200

    generate surprises

---
+++ GatherSurprises.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public final class GatherSurprises {
+
+    private static Random random;
+    private static GatherSurprises instance;
+    private static ArrayList<ISurprise> surprises;
+
+    private GatherSurprises() {
+    }
+
+    public static GatherSurprises getInstance() {
+        if (GatherSurprises.instance == null) {
+            GatherSurprises.instance = new GatherSurprises();
+        }
+        return GatherSurprises.instance;
+    }
+
+    public static ArrayList<ISurprise> gather(int n) {
+        GatherSurprises.surprises = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            GatherSurprises.surprises.add(gather());
+        }
+        return GatherSurprises.surprises;
+    }
+
+    public static ISurprise gather() {
+        if (GatherSurprises.random == null) {
+            GatherSurprises.random = new Random();
+        }
+        switch (random.nextInt(3)) {
+            case 0:
+                return FortuneCookie.generate();
+            case 1:
+                return Candies.generate();
+            case 2:
+                return MinionToy.generate();
+            default:
+                return null;
+        }
+    }
+
+    public static IBag moveToBag() {
+        IBag x = FactoryBag.getInstance().makeBag("LIFO");
+        int it;
+        if (GatherSurprises.surprises != null) {
+            it = GatherSurprises.surprises.size();
+        } else {
+            it = 0;
+        }
+        for (int i = 0; i < it; i++) {
+            x.put(GatherSurprises.surprises.get(0));
+            GatherSurprises.surprises.remove(0);
+        }
+        return x;
+    }
+
+}
