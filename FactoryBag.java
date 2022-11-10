a0337826c5f225e956ec7c58d84cfb4258ed6864
AlexandruRașa <114824311+AlexandruRasa@users.noreply.github.com>
2022-11-10 05:25:06 +0200

    create bags

---
+++ FactoryBag.java
@@ -0,0 +1,27 @@
+public final class FactoryBag implements IBagFactory {
+
+    private static FactoryBag instance;
+
+    private FactoryBag() {
+    }
+
+    public static FactoryBag getInstance() {
+        if (FactoryBag.instance == null) {
+            FactoryBag.instance = new FactoryBag();
+        }
+        return instance;
+    }
+
+    @Override
+    public IBag makeBag(String type) {
+        switch (type.toUpperCase()) {
+            case "FIFO":
+                return new FIFOBag();
+            case "LIFO":
+                return new LIFOBag();
+            default:
+                return new RandomBag();
+        }
+    }
+
+}
